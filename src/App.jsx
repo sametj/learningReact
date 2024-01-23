e0111e175767c096d7e4ea343c49334a57df2de2
sametj <mosesjames2000@gmail.com>
2024-01-22 22:37:15 -0500

    Change: Adding Title as Props/Paramater

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./header";
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header title="Amazon Cart" />
     </div>
   );
 }
--- src/header.jsx
+++ src/header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./header.css";
 
-function header() {
+function header({ title }) {
   return (
     <div className="App-header">
-      <h1 className="h1-style"> Amazon Cart</h1>
+      <h1 className="h1-style">{title}</h1>
     </div>
   );
 }
