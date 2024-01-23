61a4df730b1d38afef969417dedd89df7594c0a8
sametj <mosesjames2000@gmail.com>
2024-01-22 22:17:20 -0500

    First Commit

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function header() {
+  return (
+    <div className="App-header">
+      <h1 className="h1-style"> Amazon Cart</h1>
+    </div>
+  );
+}
+
+export default header;
